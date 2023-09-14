b3a8a82ac83780af75b330f6a2228d70a5a304e5
Ubuntu <ubuntu@ip-172-31-34-85.ec2.internal>
2023-09-14 18:15:51 +0000

    added

---
+++ strategy.py
@@ -0,0 +1,35 @@
+def implement_st_strategy(prices, st):
+    buy_price = []
+    sell_price = []
+    st_signal = []
+    signal = 0
+    
+    for i in range(len(st)):
+        if st[i-1] > prices[i-1] and st[i] < prices[i]:
+            if signal != 1:
+                buy_price.append(prices[i])
+                sell_price.append(np.nan)
+                signal = 1
+                st_signal.append(signal)
+            else:
+                buy_price.append(np.nan)
+                sell_price.append(np.nan)
+                st_signal.append(0)
+        elif st[i-1] < prices[i-1] and st[i] > prices[i]:
+            if signal != -1:
+                buy_price.append(np.nan)
+                sell_price.append(prices[i])
+                signal = -1
+                st_signal.append(signal)
+            else:
+                buy_price.append(np.nan)
+                sell_price.append(np.nan)
+                st_signal.append(0)
+        else:
+            buy_price.append(np.nan)
+            sell_price.append(np.nan)
+            st_signal.append(0)
+            
+    return buy_price, sell_price, st_signal
+
+buy_price, sell_price, st_signal = implement_st_strategy(tsla['close'], tsla['st'])
